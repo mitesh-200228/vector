dc9d8d3835ed92290451022b33cf8cd407600c8c
mitesh-200228 <81156492+mitesh-200228@users.noreply.github.com>
2024-10-26 10:46:09 +0530

    latest

--- src/MainPages/MMatcher.jsx
+++ src/MainPages/MMatcher.jsx
@@ -5,6 +5,7 @@ import {
   Text,
   Avatar,
   CircularProgress,
+  Progress,
   CircularProgressLabel,
 } from '@chakra-ui/react';
 import axios from 'axios';
@@ -13,6 +14,7 @@ const mockData = [];
 
 function Matcher() {
   const [data, setData] = useState([]);
+  const [isLoading, setLoading] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
       const datas = await axios.post(
@@ -22,16 +24,21 @@ function Matcher() {
           room_id: `${localStorage.getItem('room')}`,
         }
       );
+      setLoading(true);
       for (let i = 0; i < datas.data.linkedin_url.length; i++) {
         const name = datas.data.name[i];
         const score = datas.data.matrix.data[0][i];
-        mockData.push({name,progress:(score*100).toFixed(2),profilePic:''});
+        mockData.push({
+          name,
+          progress: (score * 100).toFixed(2),
+          profilePic: '',
+        });
       }
       const sortedData = mockData.sort((a, b) => b.progress - a.progress);
       setData(sortedData);
     };
     fetchData();
-  },[]);
+  }, []);
 
   return (
     <Box>
@@ -39,7 +46,7 @@ function Matcher() {
       <Box bg="gray.100" p={4}>
         <Flex align="center" justify="space-between" maxW="1200px" mx="auto">
           <Text fontSize="2xl" fontWeight="bold">
-            John Doe
+            Mitesh Bediya
           </Text>
           <Avatar
             size="lg"
@@ -50,36 +57,48 @@ function Matcher() {
       </Box>
 
       {/* List of potentials */}
-      <Box p={4} maxW="1200px" mx="auto">
-        {data.map((person, index) => (
-          <Flex
-            key={index}
-            align="center"
-            justify="space-between"
-            bg="white"
-            p={4}
-            mb={4}
-            borderRadius="md"
-            boxShadow="md"
-          >
-            <Flex align="center">
-              <Avatar size="md" name={person.name} src={person.profilePic} />
-              <Text fontSize="lg" fontWeight="bold" ml={4}>
-                {person.name}
-              </Text>
-            </Flex>
+      {!isLoading ? (
+        <Progress size="xs" isIndeterminate colorScheme="teal" />
+      ) : (
+        <>
+          <Box p={4} maxW="1200px" mx="auto">
+            {data.map((person, index) => (
+              <Flex
+                key={index}
+                align="center"
+                justify="space-between"
+                bg="white"
+                p={4}
+                mb={4}
+                borderRadius="md"
+                boxShadow="md"
+              >
+                <Flex align="center">
+                  <Avatar
+                    size="md"
+                    name={person.name}
+                    src={person.profilePic}
+                  />
+                  <Text fontSize="lg" fontWeight="bold" ml={4}>
+                    {person.name}
+                  </Text>
+                </Flex>
 
-            {/* Perceptage match */}
-            <CircularProgress
-              value={person.progress}
-              size="60px"
-              color="teal.400"
-            >
-              <CircularProgressLabel>{person.progress}%</CircularProgressLabel>
-            </CircularProgress>
-          </Flex>
-        ))}
-      </Box>
+                {/* Perceptage match */}
+                <CircularProgress
+                  value={person.progress}
+                  size="60px"
+                  color="teal.400"
+                >
+                  <CircularProgressLabel>
+                    {person.progress}%
+                  </CircularProgressLabel>
+                </CircularProgress>
+              </Flex>
+            ))}
+          </Box>
+        </>
+      )}
     </Box>
   );
 }
